6c91eee6504594d4f25c0bc9f294f9d605926d80
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-29 19:28:12 +0300

    Importing a picture

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -7,26 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
-    
+    var currentImage: UIImage!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
     
-    
+    @objc func importPicture(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
     @IBAction func intensityChanged(_ sender: Any) {
     }
-    
     
   
     @IBAction func changeFilter(_ sender: Any) {
     }
     
     @IBAction func save(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else {return}
+        dismiss(animated: true, completion: nil)
+        currentImage = image
     }
     
 
