0036d8ee79d4022e4caa9754eeb001fe159cda89
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-30 12:05:52 +0300

    sepia tone

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -6,16 +6,24 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
-
+    
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     var currentImage: UIImage!
+    
+    var context: CIContext!
+    var currentFilter: CIFilter!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Instafilter"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+        
     }
     
     @objc func importPicture(){
@@ -25,9 +33,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         present(picker, animated: true, completion: nil)
     }
     @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
     }
     
-  
+    
     @IBAction func changeFilter(_ sender: Any) {
     }
     
@@ -38,9 +47,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         guard let image = info[.editedImage] as? UIImage else {return}
         dismiss(animated: true, completion: nil)
         currentImage = image
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
     
-
-
+    func applyProcessing(){
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        
+        if let cgImage = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent){
+            let processedImage = UIImage(cgImage: cgImage)
+            imageView.image = processedImage
+        }
+    }
+    
+    
+    
 }
 
